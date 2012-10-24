213a5e645459618e4074fa8f99e119cf23534807
Alex Y. Wang <awang@chromatiqa.org>
2012-10-24 14:28:58 -0700

    121024

---
+++ week4/config.php
@@ -0,0 +1,4 @@
+<?php
+$link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147awang99', 'yahdogho');
+mysql_select_db('c_cs147_awang99');
+?>
--- week4/submit_.php
+++ week4/submit_.php
@@ -1,4 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Maya Online Books</title>
+		<link rel="apple-touch-icon" href="appicon.png" />
+		<link rel="apple-touch-startup-image" href="startup.png">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="viewport" content="width=device-width, user-scalable=no" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+	
+		<div class="banner"></div>
+		<?php
+		include("menu.php");
+		?>
+		
 		<div class="orderarea">
 		<?php
 		
@@ -52,4 +72,11 @@
 			
 			?>
 		</div>
-		
+		<script type="text/javascript">
+		$("a").click(function (event) {
+		    event.preventDefault();
+		    window.location = $(this).attr("href");
+		});
+		</script>
+	</body>
+</html>
